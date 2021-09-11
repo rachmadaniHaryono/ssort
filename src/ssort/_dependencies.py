9a6490630a5362b34b42073673980013465c7043
Ben Mather <bwhmather@bwhmather.com>
2021-09-11 23:06:21 +0100

    Add docstrings to dependency graph functions

--- src/ssort/_dependencies.py
+++ src/ssort/_dependencies.py
@@ -12,7 +12,16 @@ from ssort._statements import (
 
 def module_statements_graph(statements):
     """
-    Returns a dictionary mapping from statements to lists of other statements.
+    Constructs a graph of the interdependencies in a list of module level
+    statements.
+
+    :param statements:
+        An ordered list of opaque `Statement` objects from which to construct
+        the graph.
+
+    :returns:
+        A `Graph` mapping from statements to the set of statements that they
+        depend on.
     """
     # A dictionary mapping from names to the statements which bind them.
     scope = {}
@@ -80,6 +89,45 @@ def module_statements_graph(statements):
 
 
 def class_statements_initialisation_graph(statements):
+    """
+    Constructs a graph of the hard dependencies within a list of class level
+    statements.  These are dependencies that absolutely cannot be reordered
+    without changing the semantics of the script.
+
+    At inititialisation, class level statements can see earlier bindings in the
+    class body.
+
+    Note that this isn't a proper scope: variables from the containing scope are
+    not shadowed until after a binding is actually made and, obviously from the
+    need for a `self` argument, bindings are not captured.
+
+    The following example demonstrates some of this behaviour:
+
+    .. code:: python
+
+        >>> r = 1
+        >>> class A:
+        ...     a = r
+        ...     r = 2
+        ...     b = r
+        >>> r
+        1
+        >>> A.a
+        1
+        >>> A.b
+        2
+
+    If a name cannot be resolved at the class level we assume that it is
+    resolved at the module level and don't emit any warning.
+
+    :param statements:
+        An ordered list of opaque `Statement` objects from which to construct
+        the graph.
+
+    :returns:
+        A `Graph` mapping from statements to the set of statements that they
+        depend on.
+    """
     scope = {}
 
     graph = Graph()
@@ -103,6 +151,21 @@ def class_statements_initialisation_graph(statements):
 
 
 def class_statements_runtime_graph(statements):
+    """
+    Attempts to construct a graph of the soft, runtime dependencies between the
+    methods of a class.
+
+    The graph is inferred by looking for attribute access on the `self` argument
+    of methods.
+
+    :param statements:
+        An ordered list of opaque `Statement` objects from which to construct
+        the graph.
+
+    :returns:
+        A `Graph` mapping from statements to the set of statements that they
+        depend on.
+    """
     scope = {}
 
     graph = Graph()
